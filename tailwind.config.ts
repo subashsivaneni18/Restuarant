eb320df3f4c4894fc534d873f28a5034c778ef76
subashsivaneni18 <satyasaisubash8@gmail.com>
2025-05-07 15:37:57 +0530

    initial_commit

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      
+      animation: {
+        "fade-in": "fadeIn 1s ease-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+    },
+  },
+  
+  plugins: [],
+};
+
+export default config;
